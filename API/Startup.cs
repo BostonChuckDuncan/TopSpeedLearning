c7e6a67408924d660de14641b5bd843813261feb
BostonChuck <cduncan@BostonDuncan.com>
2021-07-18 13:33:17 -0400

    Fallback controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,6 +48,9 @@ namespace API
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private accountService: AccountService, private toastr: ToastrService) {    
+  }
+
+  canActivate(
+    return this.accountService.currentUser
+  }
+  
+}
